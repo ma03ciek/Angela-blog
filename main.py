6ccedf76e2fd18180b24bb4e6e8c612df95f1b01
Maciek <maciek2003strzelecki@gmail.com>
2023-01-16 23:20:42 +0100

    Dealing with PostgreSQL

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from flask_gravatar import Gravatar
 from functools import wraps
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
@@ -20,7 +20,7 @@ Bootstrap(app)
 gravatar = Gravatar(app)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///blog.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
